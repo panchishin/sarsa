bc96b33bc026b758689a172da331628b08df768c
Anton Panchishin <anton.panchishin@gmail.com>
2017-12-29 11:20:28 -0500

    rename _policy to _weights to better reflect the purpose

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ function configWithDefaults(config,defaults) {
     return cloneJSON(result)
 }
 
-function setReward(policy,state,action,reward) {
+function setReward(weights,state,action,reward) {
     state = JSON.stringify(state)
-    policy[state] = policy[state] || {}
-    policy[state][action] = reward
+    weights[state] = weights[state] || {}
+    weights[state][action] = reward
 }
 
-function getRewards(policy,state,action_list,defaultReward) {
-    var actions = policy[JSON.stringify(state)] || {}
+function getRewards(weights,state,action_list,defaultReward) {
+    var actions = weights[JSON.stringify(state)] || {}
     var result = {}
     action_list.forEach( function(action) {
         if (action in actions) {
@@ -30,19 +30,21 @@ function getRewards(policy,state,action_list,defaultReward) {
     return result
 }
 
-function sarsaEquation(state0,action0,reward1,state1,action1,alpha,gamma,policy,defaultReward,getRewards,setReward) {
+function sarsaEquation(state0,action0,reward1,state1,action1,alpha,gamma,weights,defaultReward,getRewards,setReward) {
     // formula : ( 1 - a )Q(t) + (a)( r + yQ(t+1) )
 
     var a = alpha
-    var Qt0 = getRewards(policy,state0,[action0],defaultReward)[action0]
-    var Qt1 = getRewards(policy,state1,[action1],defaultReward)[action1]
+    var Qt0 = getRewards(weights,state0,[action0],defaultReward)[action0]
+    var Qt1 = getRewards(weights,state1,[action1],defaultReward)[action1]
     var r = reward1
     var y = gamma
 
     var result = (1-a)*Qt0 + a*(r+y*Qt1)
-    setReward(policy,state0,action0,result)
+    setReward(weights,state0,action0,result)
     return result
 }
+
+
 
 var defaults = {
     'alpha' : 0.2,     // default to a low(-ish) learning rate
@@ -54,7 +56,7 @@ module.exports = function sarsaConstructor(config) {
 
     return {
         _config : configWithDefaults(config,defaults),
-        _policy : {},
+        _weights : {},
         getConfig : function() {
             return cloneJSON(this._config)
         },
@@ -63,20 +65,20 @@ module.exports = function sarsaConstructor(config) {
             return this
         },
         setReward : function(state,action,reward) {
-            setReward(this._policy,state,action,reward)
+            setReward(this._weights,state,action,reward)
             return this
         },
         getRewards : function(state,action_list) {
-            return cloneJSON(getRewards(this._policy,state,action_list,this._config.defaultReward))
+            return cloneJSON(getRewards(this._weights,state,action_list,this._config.defaultReward))
         },
         update : function (state0,action0,reward1,state1,action1) {
             return sarsaEquation(state0,action0,reward1,state1,action1,
                 this._config.alpha,this._config.gamma,
-                this._policy,this._config.defaultReward,getRewards,setReward)
+                this._weights,this._config.defaultReward,getRewards,setReward)
         },
         clone : function() {
             var clone = sarsaConstructor(this._config)
-            clone._policy = cloneJSON(this._policy)
+            clone._weights = cloneJSON(this._weights)
             return clone
         }
     }
